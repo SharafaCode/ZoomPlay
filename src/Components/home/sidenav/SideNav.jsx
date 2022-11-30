a1f40165a2adb4d92fe6492a509cd615c390352a
sharafa bashiru <sharafaoluwatobi@gmail.com>
2022-11-30 12:16:00 +0100

    side nav added

--- src/Components/home/sidenav/SideNav.jsx
+++ src/Components/home/sidenav/SideNav.jsx
@@ -1,8 +1,90 @@
-import React from 'react'
-
+import React from 'react';
+import './sidenav.css';
+import { RiHome2Fill } from "react-icons/ri";
+import {MdPerson, MdVideoLibrary } from "react-icons/md";
+import { BsFillCollectionPlayFill } from "react-icons/bs";
+import { HiRadio } from "react-icons/hi2";
+import { IoLogOut } from "react-icons/io5";
+// import HomeIcon from '../../../assets/Music Image/Home icon.png'
+// import PlayListIcon from '../../../assets/Music Image/playlist icon.png';
+// import RadioIcon from '../../../assets/Music Image/radio icon.png';
+// import VideoIcon from '../../../assets/Music Image/videos icon.png';
+// import ProfileIcon from '../../../assets/Music Image/profile icon.png';
+// import LogoutIcon from '../../../assets/Music Image/logout icon.png';
 const SideNav = () => {
   return (
-    <div>SideNav</div>
+    <aside className='aside-container'>
+      <article className='article-wrapper'>
+        <div className="content-wrapper">
+
+          <div className='links'>
+
+          <a href="">
+            <span className='icons active-icon'>
+              < RiHome2Fill/>
+            </span>
+            <span className='active'>
+              <h1>Home</h1>
+            </span>
+          </a>
+          <a href="">
+            <span className='icons'>
+              <BsFillCollectionPlayFill/>
+            </span>
+            <span>
+              <h1>
+              PLaylist
+              </h1>
+            </span>
+          </a>
+          <a href="">
+            <span className='icons'>
+              <HiRadio/>
+            </span>
+            <span>
+              <h1>
+              Radio
+              </h1>
+            </span>
+          </a>
+          <a href="">
+            <span className='icons'>
+              <MdVideoLibrary/>
+            </span>
+            <span>
+              <h1>
+
+              Music Video
+              </h1>
+            </span>
+          </a>
+          <a href="">
+            <span className='icons'>
+              < MdPerson/>
+            </span>
+            <span>
+              <h1>
+
+              Profile
+              </h1>
+            </span>
+          </a>
+          <a href="">
+            <span className='icons'>
+              <IoLogOut />
+            </span>
+            <span>
+              <h1>
+
+              Logout
+              </h1>
+            </span>
+          </a>
+          </div>
+        </div>
+
+      </article>
+    </aside>
   )
 }
 
--- src/Components/section/Section.jsx
+++ src/Components/section/Section.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from '../header/Header'
+import SideNav from '../home/sidenav/SideNav'
 
 const Section = () => {
   return (
     <section>
         <article>
+          <div>
+
             <Header/>
+          </div>
+          <div>
+            <SideNav/>
+          </div>
             <div className="section-content">
               
             </div>

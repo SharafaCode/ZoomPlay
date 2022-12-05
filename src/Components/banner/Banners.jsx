592101c908f416ec41a83d3bd8265d096baf0c74
sharafa bashiru <sharafaoluwatobi@gmail.com>
2022-12-05 22:00:09 +0100

    change made

--- src/Components/banner/Banners.jsx
+++ src/Components/banner/Banners.jsx
@@ -5,24 +5,36 @@ import './banner.css'
 
 const Banners = () => {
     
-    const [data, setData] = useState(BannerData)
+    const [data, setData] = useState(BannerData);
+    const [dataindex, setDataIndex] = useState(0)
     console.log(data);
   return (
     <section className='banner-section'>
         <article className='banner-article'>
           
             {
-                data.map((item)=>{
+                data.map((item, index)=>{
                     const {id, imag} = item;
+                    let position = "next-banner"
+                    if(index === dataindex){
+                        position = "active-banner"
+                    }
+                    if(index === BannerData.length - 1){
+                        position = "last-banner"
+                    }
                     return(
-                        <div className="banner" key={id}>
-                            <div className="banners">
-                                <img src={imag} alt="" />
+                        <div className={`banner`} key={id}>
+                            <div className={`banners `}>
+                                <img src={imag} alt="" className={`${position}`} />
                             </div>
                         </div>
                     )
                 })
             }
+
+            <div className='btn' onClick={()=> setDataIndex(dataindex + 1)}>
+                <button>go</button>
+            </div>
 
         </article>
     </section>

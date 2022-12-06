283a1b329116362738772874901e34a8cc19f9d8
sharafa bashiru <sharafaoluwatobi@gmail.com>
2022-12-06 04:40:38 +0100

    news section added

--- src/Components/banner/Banners.jsx
+++ src/Components/banner/Banners.jsx
@@ -1,43 +1,37 @@
 import React from 'react'
 import { useState } from 'react';
-import BannerData from './bannerdata';
+// import BannerData from './bannerdata';
 import './banner.css'
 
 const Banners = () => {
     
-    const [data, setData] = useState(BannerData);
-    const [dataindex, setDataIndex] = useState(0)
-    console.log(data);
+    // const [data, setData] = useState(BannerData);
+    // const [dataindex, setDataIndex] = useState(0)
+    // console.log(data);
   return (
+
     <section className='banner-section'>
         <article className='banner-article'>
-          
-            {
-                data.map((item, index)=>{
-                    const {id, imag} = item;
-                    let position = "next-banner"
-                    if(index === dataindex){
-                        position = "active-banner"
-                    }
-                    if(index === BannerData.length - 1){
-                        position = "last-banner"
-                    }
-                    return(
-                        <div className={`banner`} key={id}>
-                            <div className={`banners `}>
-                                <img src={imag} alt="" className={`${position}`} />
-                            </div>
-                        </div>
-                    )
-                })
-            }
-
-            <div className='btn' onClick={()=> setDataIndex(dataindex + 1)}>
-                <button>go</button>
+            <div className="banner-content">
+               <div className='artise-name'>
+                <h1>
+                    Wizkid
+                </h1>
+               </div>
+               <div className='artise-details'>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos consequuntur sit, corporis officiis optio molestias.
+                </p>
+               </div>
+               <div className='bttns'>
+                <button className='buttn'>PLAY</button>
+                <button className='buttn'>DOWNLOAD</button>
+               </div>
             </div>
 
         </article>
     </section>
+
   )
 }
 
